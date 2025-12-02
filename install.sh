1bc24a806e1140994d9f1e3b31fc7c0a42b855c9
MadMax182 <120349835+MadMax182@users.noreply.github.com>
2025-12-02 11:35:13 -0600

    install script

--- install.sh
+++ install.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-sudo cp ./hypr/keybinds.conf ~/.config/hypr/conf/keybindings/custom-keybinds.conf
+#sets the working directory to the scripts directory
+cd "$(dirname "$0")"
+
+echo "Current working directory is now: $(pwd)"
+
+echo "setting keybinds"
+sudo cp ./hypr/keybinds.conf $HOME/.config/hypr/conf/keybindings/default.conf
+
