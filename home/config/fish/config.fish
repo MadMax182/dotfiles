c86c3121c378c7454bffd8bfbbaa0323391741bc
MadMax182 <120349835+MadMax182@users.noreply.github.com>
2025-12-27 16:14:09 -0600

    update

--- home/config/fish/config.fish
+++ home/config/fish/config.fish
@@ -6,3 +6,8 @@ fish_add_path ~/.local/bin
 
 set -gx SSH_AUTH_SOCK ~/.1password/agent.sock
 set -gx EDITOR nvim
+
+# Added by LM Studio CLI (lms)
+set -gx PATH $PATH /home/mmzim/.lmstudio/bin
+# End of LM Studio CLI section
+
